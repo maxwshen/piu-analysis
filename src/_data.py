f84695552d92412426d116200d72ff6b1d6730e6
maxwshen <maxwshen@gmail.com>
2020-07-22 01:04:51 -0400

    stepchart gathering

--- src/_data.py
+++ src/_data.py
@@ -1,8 +1,9 @@
 #
 import _config
 from collections import defaultdict
-import pandas as pd
+import numpy as np, pandas as pd
 import os
+from typing import List, Dict, Set, Tuple
 
 stepf2_fold = '/mnt/c/Users/maxws/Downloads/StepF2/Songs/'
 
@@ -50,7 +51,6 @@ stepf2_df = pd.read_csv(stepf2_df_fn, index_col = 0)
 
 standard_packs = [
   '1ST~3RD',
-  'BasicModeGroup',
   'EXCEED~ZERO',
   'FIESTA 2',
   'FIESTA EX',
@@ -61,12 +61,13 @@ standard_packs = [
   'NX2',
   'PRIME 2',
   'PRIME',
-  'PRIME2 [BETA]',
   'PRO~PRO2',
   'REBIRTH~PREX 3',
   'S.E.~EXTRA',
-  # 'SKILLUP ZONE',
   'XX',
+  # 'BasicModeGroup',
+  # 'SKILLUP ZONE',
+  # 'PRIME2 [BETA]',
 ]
 
 modern_packs = [
@@ -80,8 +81,9 @@ modern_packs = [
 datasets = {
   'all': stepf2_df,
   'standard': stepf2_df[stepf2_df['Pack'].isin(standard_packs)],
-  'postf': stepf2_df[stepf2_df['Pack'].isin(modern_packs)],
+  'fiesta2_onwards': stepf2_df[stepf2_df['Pack'].isin(modern_packs)],
 }
+
 
 '''
   .ssc class
@@ -92,24 +94,21 @@ class SSCFile():
     https://github.com/stepmania/stepmania/wiki/ssc
     https://github.com/stepmania/stepmania/wiki/sm
   '''
-  def __init__(self, ssc_fn):
+  def __init__(self, ssc_fn, pack = ''):
     self.ssc_fn = ssc_fn
-    self.attributes = dict()
-    self.stepcharts = []
-
     lines = open(ssc_fn, 'r').readlines()
-    file_sections = self.__parse_file_sections(lines)
+    sections = self.__parse_file_sections(lines)
 
-    self.attributes = self.__parse_attributes(file_sections['global_header'])
-    self.__parse_stepcharts(file_sections['stepcharts'])
-
-    # TODO: Make basic stats dict
+    self.global_attributes = self.__parse_attributes(sections['global_header'])
+    self.global_attributes['Pack'] = pack
+    self.sc_attributes, self.sc_notes = self.__parse_stepcharts(sections['stepcharts'])
     pass
+
 
   '''
     Parsing
   '''
-  def __parse_file_sections(self, lines):
+  def __parse_file_sections(self, lines: str) -> dict:
     '''
       Delineated by #NOTEDATA:;
       Returns fs = {
@@ -130,17 +129,27 @@ class SSCFile():
       'stepcharts': sections[1:],
     }
 
-  def __parse_stepcharts(self, stepcharts):
+
+  def __parse_stepcharts(self, stepcharts: list) -> list:
     delim = '#NOTES:\n'
+    sc_atts = []
+    all_notes = []
     for stepchart in stepcharts:
       [header, notes] = stepchart.split(delim)[:2]
       attributes = self.__parse_attributes(header)
-      stepchart_nm = self.__stepchart_name(attributes)
-      notes = self.__parse_notes()
-    # TODO: Aggregate attribute and notes across stepcharts and return something
-    return
+      self.__annotate_stepchart(attributes)
+      sc_atts.append(attributes)
 
-  def __parse_attributes(self, header):
+      if '\n\n' in notes:
+        notes = notes.replace('\n\n', '\n')
+      notes = notes[:notes.index(';')]
+      notes = notes.strip()
+      all_notes.append(notes)
+
+    return sc_atts, all_notes
+
+
+  def __parse_attributes(self, header: str) -> dict:
     '''
       Input header is a string with \n
   
@@ -155,27 +164,110 @@ class SSCFile():
       kv = remove_comments(kv)
       if ':' in kv:
         [key, val] = kv.split(':')[:2]
+        if key[0] == '#':
+          key = key[1:]
         atts[key] = val
     return atts
 
-  def __parse_notes(self):
-    '''
-      TODO
-    '''
-
-    return
 
   '''
    Support
   '''
-  def __stepchart_name(self, atts):
+  def __annotate_stepchart(self, atts: dict):
     '''
+      Annotates custom stepchart attributes, using description and other attributes
     '''
-    getter = lambda q: atts[q] if q in atts else ''
-    stepstype = getter('STEPSTYPE')
-    level = getter('METER')
-    desc = getter('DESCRIPTION')
-    chartname = getter('CHARTNAME')
-    name = f'{stepstype} {level} {desc} {chartname}'
-    return name
+    get_att = lambda q: atts[q] if q in atts else '-'
+    song_nm = self.global_attributes['TITLE']
+    stepstype = get_att('STEPSTYPE')
+    bpm = get_att('BPMS')
+    level = get_att('METER')
+    desc = get_att('DESCRIPTION').upper()
+    songtype = self.global_attributes['SONGTYPE'].lower()
+    artist = self.global_attributes['ARTIST']
 
+    if stepstype == 'pump-single':
+      st_short = 'S'
+    elif stepstype == 'pump-double':
+      st_short = 'D'
+    elif stepstype == 'pump-halfdouble':
+      st_short = 'HD'
+    else:
+      st_short = '?'
+
+    atts['Is UCS'] = bool('UCS' in desc)
+    atts['Is quest'] = bool('QUEST' in desc)
+    atts['Is hidden'] = bool('HIDDEN' in desc)
+    atts['Is halfdouble'] = bool('HALF' in desc)
+    if atts['Is halfdouble']:
+      st_short = 'HD'
+      atts['STEPSTYPE'] = 'pump-halfdouble'
+    atts['Is SP'] = bool('SP' in desc)
+    atts['Is DP'] = bool('DP' in desc)
+    if atts['Is SP'] or atts['Is DP']:
+      st_short += 'P'
+    atts['Is infinity'] = bool('INFINITY' in desc)
+    atts['Is train'] = bool('TRAIN' in desc)
+    atts['First BPM'] = bpm.split(',')[0]
+
+    atts['Name'] = f'{song_nm} - {artist} {st_short}{level} {songtype}'
+    name_modifiers = ['Is quest', 'Is hidden', 'Is infinity', 'Is train']
+    for name_mod in name_modifiers:
+      if atts[name_mod]:
+        nm_mod = name_mod.replace('Is ', '')
+        atts['Name'] += f' {nm_mod}'
+
+    atts['Steptype simple'] = st_short
+    return
+
+
+  '''
+    Public
+  '''
+  def get_stepchart_info(self) -> pd.DataFrame:
+    cols = [
+      'Name',
+      'Steptype simple',
+      'Is UCS',
+      'Is quest',
+      'Is hidden',
+      'Is infinity',
+      'Is train',
+      'Pack',
+      'VERSION',
+      'TITLE',
+      'SUBTITLE',
+      'ARTIST',
+      'GENRE',
+      'CREDIT',
+      'OFFSET',
+      'SELECTABLE',
+      'SONGTYPE',
+      'SONGCATEGORY',
+      'TIMESIGNATURES',
+      'STEPSTYPE',
+      'DESCRIPTION',
+      'DIFFICULTY',
+      'METER',
+      'DISPLAYBPM',
+      'First BPM',
+    ]
+    '''
+      Each row is a stepchart. Columns are stepchart-specific attributes and global attributes
+    '''
+    dd = defaultdict(list)
+    for sc_att in self.sc_attributes:
+      for key in cols:
+        if key in sc_att:
+          dd[key].append(sc_att[key])
+        elif key in self.global_attributes:
+          dd[key].append(self.global_attributes[key])
+        else:
+          dd[key].append(np.nan)
+    df = pd.DataFrame(dd)
+    return df
+
+  def get_stepchart_notes(self) -> List[str]:
+    '''
+    '''
+    return self.sc_notes
--- src/a_format_data.py
+++ src/a_format_data.py
@@ -1,9 +1,9 @@
 # 
 from __future__ import division
-import _config, _data, util
+import _config, _data, util, pickle
 import sys, os, fnmatch, datetime, subprocess
 import numpy as np
-from collections import defaultdict
+from collections import defaultdict, Counter
 import pandas as pd
 
 # Default params
@@ -15,20 +15,108 @@ util.ensure_dir_exists(out_dir)
 ##
 # Functions
 ##
-def format_data(ssc_fn):
-
-  return
-
-
-def import_all_sscs():
+def get_all_stepcharts_df() -> pd.DataFrame:
+  '''
+    Loads and filters stepcharts from .ssc files
+    Outputs
+    - CSV describing each stepchart
+    - Dict mapping stepchart name to notes string
+  '''
   df = _data.datasets['all']
   print(f'Importing {len(df)} .ssc files ...')
+  mdf = pd.DataFrame()
+  all_notes = []
   timer = util.Timer(total = len(df))
   for idx, row in df.iterrows():
     ssc_fn = row['Files']
-    sc = _data.SSCFile(ssc_fn)
+    sc = _data.SSCFile(ssc_fn, pack = row['Pack'])
+
+    sc_df = sc.get_stepchart_info()
+    mdf = mdf.append(sc_df, ignore_index = True)
+
+    # Same order as mdf
+    sc_notes = sc.get_stepchart_notes()
+    all_notes += sc_notes
+
     timer.update()
-  return
+  mdf['Stepchart index'] = mdf.index
+  print(f'Found {len(mdf)} total stepcharts ...')
+
+
+  # Filter down to singles and doubles only
+  ok_steptypes = ['pump-single', 'pump-double', 'pump-halfdouble']
+  print(f'Filtering down to {ok_steptypes}...')
+  crit = (mdf['STEPSTYPE'].isin(ok_steptypes))
+  mdf = mdf[crit].reset_index(drop = True)
+  print(f'... retained {len(mdf)} stepcharts ...')
+
+  # Filter UCS out
+  print(f'Filtering down to non-UCS...')
+  crit = (mdf['Is UCS'] == False)
+  mdf = mdf[crit].reset_index(drop = True)
+  print(f'... retained {len(mdf)} stepcharts ...')
+
+  # Filter to standard packs -- some repeats in basic mode
+  print(f'Filtering down to standard packs...')
+  crit = (mdf['Pack'].isin(_data.standard_packs))
+  mdf = mdf[crit].reset_index(drop = True)
+  print(f'... retained {len(mdf)} stepcharts ...')
+
+  # Filter quest
+  print(f'Filtering quest stepcharts...')
+  crit = (mdf['Is quest'] == False)
+  mdf = mdf[crit].reset_index(drop = True)
+  print(f'... retained {len(mdf)} stepcharts ...')
+
+  # Filter special
+  print(f'Filtering special stepcharts...')
+  crit = (mdf['SONGTYPE'] != 'SPECIAL')
+  mdf = mdf[crit].reset_index(drop = True)
+  print(f'... retained {len(mdf)} stepcharts ...')
+
+  # Look at non-unique names
+  nmcts = Counter(mdf['Name'])
+  dup_nms = sorted(nmcts, key = nmcts.get, reverse = True)
+  dup_nms = [s for s in dup_nms if nmcts[s] > 1]
+  # res = [(s, nmcts[s]) for s in dup_nms]
+
+  # Make names unique
+  num_unique_nms = len(set(mdf['Name']))
+  print(f'Found {num_unique_nms} unique names')
+
+  print(f'Making names unique ...')
+  seen = defaultdict(lambda: 0)
+  dup_nms = set(dup_nms)
+  nms = list(mdf['Name'])
+  for idx, nm in enumerate(nms):
+    if nm not in dup_nms:
+      continue
+    seen[nm] += 1
+    nms[idx] = f'{nm} v{seen[nm]}'
+  mdf['Name'] = nms
+  mdf = mdf.rename(columns = {'Name': 'Name (unique)'})
+
+  num_unique_nms = len(set(mdf['Name (unique)']))
+  print(f'Found {num_unique_nms} unique names')
+  assert num_unique_nms == len(mdf)
+
+  # Save
+  print(f'Total: {len(mdf)} stepcharts ...')
+  mdf.to_csv(out_dir + f'all_stepcharts.csv')
+
+  # Handle notes -- filter, then convert to dict
+  all_notes_d = {}
+  print(f'Filtering notes ...')
+  timer = util.Timer(total = len(mdf))
+  for idx, row in mdf.iterrows():
+    notes = all_notes[row['Stepchart index']]
+    all_notes_d[row['Name (unique)']] = notes
+    timer.update()
+
+  with open(out_dir + f'notes.pkl', 'wb') as f:
+    pickle.dump(all_notes_d, f)
+
+  return mdf
 
 
 ##
@@ -69,16 +157,16 @@ def gen_qsubs():
 def main():
   print(NAME)
   
-  # _data.crawl_all_ssc()
+  get_all_stepcharts_df()
 
-  test_ssc_fn = '/mnt/c/Users/maxws/Downloads/StepF2/Songs/19-XX/(00) 1608 - MAX - I Want U/1608 - MAX - I Want U.ssc'
-  sc = _data.SSCFile(test_ssc_fn)
+  # test_ssc_fn = '/mnt/c/Users/maxws/Downloads/StepF2/Songs/19-XX/(00) 1608 - MAX - I Want U/1608 - MAX - I Want U.ssc'
+  # sc = _data.SSCFile(test_ssc_fn)
 
-  # import_all_sscs()
+  # # import_all_sscs()
 
-  format_data(
-    ssc_fn = _config.SRC_DIR + 'timeforthemoonnight-s18.ssc'
-  )
+  # format_data(
+  #   ssc_fn = _config.SRC_DIR + 'timeforthemoonnight-s18.ssc'
+  # )
 
   return
 
