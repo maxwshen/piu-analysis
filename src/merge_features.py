a66d368cb975a90ff1cc2890ed69ddd5f97767ad
maxwshen <maxwshen@gmail.com>
2021-06-25 13:01:25 -0400

    add timer to merge

--- src/merge_features.py
+++ src/merge_features.py
@@ -1,4 +1,4 @@
-import _config
+import _config, util
 import os
 import pandas as pd
 
@@ -7,10 +7,12 @@ inp_dir = _config.OUT_PLACE + 'd_annotate/'
 def main():
   fns = [fn for fn in os.listdir(inp_dir) if 'features' in fn]
 
+  timer = util.Timer(total=len(fns))
   mdf = pd.DataFrame()
   for fn in fns:
     df = pd.read_csv(inp_dir + fn, index_col=0)
     mdf = mdf.append(df)
+    timer.update()
   
   mdf.to_csv(inp_dir + f'features.csv')
   return
