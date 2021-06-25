b3d7d7a1d7a1047c41b8aaf8e9ca6639ebcbbe33
maxwshen <maxwshen@gmail.com>
2021-06-25 13:00:14 -0400

    merge

---
+++ src/merge_features.py
@@ -0,0 +1,20 @@
+import _config
+import os
+import pandas as pd
+
+inp_dir = _config.OUT_PLACE + 'd_annotate/'
+
+def main():
+  fns = [fn for fn in os.listdir(inp_dir) if 'features' in fn]
+
+  mdf = pd.DataFrame()
+  for fn in fns:
+    df = pd.read_csv(fn)
+    mdf = mdf.append(df)
+  
+  mdf.to_csv(inp_dir + f'features.csv')
+  return
+
+
+if __name__ == '__main__':
+  main()
