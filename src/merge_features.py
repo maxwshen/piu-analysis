7e6e85ff65af8444bb00f6bab7cebd9341a04372
maxwshen <maxwshen@gmail.com>
2021-06-28 23:28:41 -0400

    qsub for merge

--- src/merge_features.py
+++ src/merge_features.py
@@ -1,23 +1,94 @@
 import _config, util
-import os
+import sys, os, subprocess
 import pandas as pd
 
 inp_dir = _config.OUT_PLACE + 'd_annotate/'
+NAME = util.get_fn(__file__)
+out_dir = _config.OUT_PLACE + NAME + '/'
+util.ensure_dir_exists(out_dir)
 
+fns = [fn for fn in os.listdir(inp_dir) if '_features' in fn]
+num_jobs = 20
 
-def main():
-  fns = [fn for fn in os.listdir(inp_dir) if '_features' in fn]
+def merge(start, end):
+  start, end = int(start), int(end)
 
-  timer = util.Timer(total=len(fns))
+  sub_fns = fns[start:end]
+
+  timer = util.Timer(total=len(sub_fns))
   mdf = pd.DataFrame()
-  for fn in fns:
+  for fn in sub_fns:
     df = pd.read_csv(inp_dir + fn, index_col=0)
     mdf = mdf.append(df)
     timer.update()
   
-  mdf.to_csv(_config.OUT_PLACE + f'features.csv')
+  mdf.to_csv(out_dir + f'features-{start}-{end}.csv')
+  return
+
+
+def merge_all():
+  mdf = pd.DataFrame()
+  num_per_run = get_num_per_run()
+  timer = util.Timer(total=num_jobs)
+  for start in range(0, len(fns), num_per_run):
+    end = start + num_per_run
+    df = pd.read_csv(out_dir + f'features-{start}-{end}.csv', index_col=0)
+    mdf = mdf.append(df)
+    timer.update()
+  mdf.to_csv(out_dir + 'features.csv')
+  return
+
+
+def get_num_per_run():
+  num_per_run = len(fns) // num_jobs
+  if num_per_run * num_jobs < len(fns):
+    num_per_run += 1
+  return num_per_run
+
+#
+def gen_qsubs():
+  print('Generating qsub scripts...')
+  qsubs_dir = _config.QSUBS_DIR + NAME + '/'
+  util.ensure_dir_exists(qsubs_dir)
+  qsub_commands = []
+
+  script_id = NAME.split('_')[0]
+
+  num_per_run = get_num_per_run()
+
+  num_scripts = 0
+  for start in range(0, len(fns), num_per_run):
+    end = start + num_per_run
+    command = f'python {NAME}.py run_single {start} {end}'
+
+    # Write shell scripts
+    sh_fn = qsubs_dir + f'q_{script_id}_{start}_{end}.sh'
+    with open(sh_fn, 'w') as f:
+      f.write(f'#!/bin/bash\n{command}\n')
+    num_scripts += 1
+
+    qsub_commands.append(f'qsub -j y -V -P regevlab -l h_rt=1:00:00, -wd {_config.SRC_DIR} {sh_fn} &')
+
+  # Save commands
+  commands_fn = qsubs_dir + '_commands.sh'
+  with open(commands_fn, 'w') as f:
+    f.write('\n'.join(qsub_commands))
+  subprocess.check_output(f'chmod +x {commands_fn}', shell = True)
+  
+  print(f'Wrote {num_scripts} shell scripts to {qsubs_dir}')
   return
 
 
 if __name__ == '__main__':
-  main()
+  if len(sys.argv) == 1:
+    print('-- Intended to run with qsub')
+  else:
+    if sys.argv[1] == 'gen_qsubs':
+      gen_qsubs()
+    elif sys.argv[1] == 'run_single':
+      merge(
+        start = sys.argv[2],
+        end = sys.argv[3]
+      )
+    elif sys.argv[1] == 'merge_all':
+      merge_all()
