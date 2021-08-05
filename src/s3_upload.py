21b318bbb6506c9bc3b38773b3f1232275447b09
Max Shen <maxwshen@gmail.com>
2021-08-04 22:30:43 -0400

    s3 upload single

--- src/s3_upload.py
+++ src/s3_upload.py
@@ -1,13 +1,16 @@
 '''
   Upload files to S3, to be downloaded by the app
 '''
+import sys, os
 import _config, util, string
-import os
 import boto3
 
 s3 = boto3.client('s3', 
                   aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], 
                   aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+
+E_STRUCT_FOLD = _config.OUT_PLACE + 'e_struct/'
+
 
 # Upload single files
 single_fns = {
@@ -26,13 +29,32 @@ def s3_safe(name):
   return ''.join(convert_safe(char) for char in name)
 
 
-# Upload e_struct files per stepchart
-e_struct_fold = _config.OUT_PLACE + 'e_struct/'
-files = [fn for fn in os.listdir(e_struct_fold) if '.pkl' in fn]
-print(f'Uploading {len(files)} e_struct files ...')
-timer = util.Timer(total=len(files))
-for fn in files:
-  s3.upload_file(e_struct_fold + fn, os.environ['S3_BUCKET_NAME'], s3_safe(fn))
-  timer.update()
+'''
+  Upload stepchart pkls
+'''
+def upload_all():
+  # Upload e_struct files per stepchart
+  files = [fn for fn in os.listdir(E_STRUCT_FOLD) if '.pkl' in fn]
+  print(f'Uploading {len(files)} e_struct files ...')
+  timer = util.Timer(total=len(files))
+  for fn in files:
+    s3.upload_file(E_STRUCT_FOLD + fn, os.environ['S3_BUCKET_NAME'], s3_safe(fn))
+    timer.update()
 
-print(f'Done')
+  print(f'Done')
+  return
+
+
+def upload_single():
+  nm = 'Headless Chicken - r300k D21 arcade'
+  # nm = 'Trashy Innocence - Last Note. D15 arcade'
+  fn = nm + '.pkl'
+  s3.upload_file(E_STRUCT_FOLD + fn, os.environ['S3_BUCKET_NAME'], s3_safe(fn))
+  return
+
+
+if __name__ == '__main__':
+  if sys.argv[1] == 'all':
+    upload_all()
+  elif sys.argv[1] == 'single':
+    upload_single()
